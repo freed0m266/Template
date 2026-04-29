83a492ae4b0fb58bc89111623841a9c2b922e949
Martin Svoboda <martin.svoboda@eucdigitalis.cz>
2026-04-29 16:48:00 +0200

    💄 Add some gradient for ContentView

--- Template/Sources/Views/ContentView.swift
+++ Template/Sources/Views/ContentView.swift
@@ -11,6 +11,22 @@ import SwiftUI
 struct ContentView: View {
 	var body: some View {
 		Text("Hello, World!")
+			.font(.title2.weight(.semibold))
+			.foregroundStyle(.white)
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.background {
+				RadialGradient(
+					colors: [
+						Color(red: 40/255, green: 92/255, blue: 35/255).opacity(0.65),
+						Color(red: 40/255, green: 92/255, blue: 35/255).opacity(0.18),
+						.black.opacity(0.0)
+					],
+					center: .topLeading,
+					startRadius: 40,
+					endRadius: 500
+				)
+				.ignoresSafeArea()
+			}
 	}
 }
 
